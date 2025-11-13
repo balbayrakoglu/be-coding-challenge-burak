ab819b814970a98b87d74ed853543a05ff5e2c85
Burak Albayrakoglu <balbayrakoglu@gmail.com>
2025-11-13 01:54:40 +0100

    RetraybleTopic and dlq comment out on purpose.

--- src/main/kotlin/de/dkb/api/codeChallenge/notification/messaging/kafka/NotificationSubscriber.kt
+++ src/main/kotlin/de/dkb/api/codeChallenge/notification/messaging/kafka/NotificationSubscriber.kt
@@ -2,18 +2,35 @@ package de.dkb.api.codeChallenge.notification.messaging.kafka
 
 import de.dkb.api.codeChallenge.notification.model.dto.NotificationDto
 import de.dkb.api.codeChallenge.notification.service.NotificationService
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.annotation.RetryableTopic
+import org.springframework.retry.annotation.Backoff
 import org.springframework.stereotype.Component
 
 @Component
-class NotificationSubscriber(private val notificationService: NotificationService) {
+class NotificationSubscriber(
+    private val notificationService: NotificationService
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
 
-    // For the challenge this is deactivated, but consider this functionality of a heavy use in the real life system
+/*    @RetryableTopic(
+        attempts = "3",
+        backoff = Backoff(delay = 2000),
+        dltTopicSuffix = "-dlq",
+        autoCreateTopics = "true"
+    )*/
     @KafkaListener(
         topics = ["notifications"],
         groupId = "codechallenge_group",
         autoStartup = "\${kafka.listener.enabled:false}"
     )
-    fun consumeNotification(notificationDto: NotificationDto) =
-        notificationService.sendNotification(notificationDto)
+    fun consumeNotification(notificationDto: NotificationDto) {
+        try {
+            notificationService.sendNotification(notificationDto)
+        } catch (ex: Exception) {
+            log.error("sendNotification failed for message: {}", notificationDto, ex)
+            throw ex
+        }
+    }
 }
